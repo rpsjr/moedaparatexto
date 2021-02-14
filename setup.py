0614055f27e57e2400a047655827877237adddf0
RPSJR <raimundops.jr@gmail.com>
2021-02-14 15:32:29 -0300

    Add existing file

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='moedaparatexto',
+      version='0.1',
+      description='Escrivão de valores de moeda por extenso',
+      url='http://github.com/rpsjr/moedaparatexto',
+      author='Raimundo Pereira da Silva Junior',
+      author_email='raimundopsjr@gmail.com',
+      license='MIT',
+      packages=['moedaparatexto'],
+      zip_safe=False)
